a54501c8ab8ccc340cc324f6c621be1bf2bc00f6
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-12-30 20:37:01 -0600

    Update esportsParseTest.py

--- esportsParseTest.py
+++ esportsParseTest.py
@@ -1,32 +1,34 @@
 import leaguepedia_parser as lp
 import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
 # Joshua Prila
 # Use this for learning purposes
-
-pp = pprint.PrettyPrinter(indent=4)
 
 # Gets regions names
 regions = lp.get_regions()
 # print(regions)
 # ['', 'Africa', 'Asia', 'Brazil', 'China', 'CIS', 'Europe', 'International', 'Japan', 'Korea', 'LAN', 'LAS', 'Latin America', 'LMS', 'MENA', 'North America', 'Oceania', 'PCS', 'SEA', 'Turkey', 'Unknown', 'Vietnam', 'Wildcard']
 
-# get_tournaments takes in string and integer, returns json dict (?) for containing info for all tournaments
+# get_tournaments takes in string and integer, returns json object (just like python dictionaries) for containing info for all tournaments
 # NA example: Spring split, Spring playoffs, Summer split, Summer playoffs
 # Use overviewPage to obtain game information
-tournaments = lp.get_tournaments("North America", year=2020)
+tournaments = lp.get_tournaments("International", year=2020)
 
-# Here we can fill an array with overviewPage information for all tournaments in North America, for the 2020 season
+# Here we can fill an array with overviewPage information for all tournaments Internationally, for the 2020 season
 tourney = []
 for x in tournaments: 
     tourney.append(x["overviewPage"])
 
-tourneyIDX = tourney.index("LCS/2020 Season/Summer Playoffs") # TourneyIDX is idx of desired tournament
-# In this case, our desired tournament is "LCS/2020 Season/Summer Playoffs"
+tourneyIDX = tourney.index("2020 Season World Championship/Main Event") # TourneyIDX is idx of desired tournament
+# In this case, our desired tournament is "2020 Season World Championship/Main Event"
+# Another example is "LCS/2020 Season/Summer Playoffs, for region "North America"."
 
 games = lp.get_games(tourney[tourneyIDX])
-game = lp.get_game_details(games[0]) 
-# games[0] respresents TSM vs GG game 1 of the 2020 LCS Summer Playoffs (the first game played of the LCS Summer Playoffs)
-# Let's print out the stats of this game
+game = lp.get_game_details(games[-1]) 
+# games[-1] respresents the last professional game of 2020, which is the final game of the 2020 World Championship Finals
+
+# Let's print out the stats of this game:
 
 pp.pprint(game)
 

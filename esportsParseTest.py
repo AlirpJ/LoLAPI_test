b2a557b0e43b42ab65daefda3bd9077b614f219b
Joshua Prila <55035345+AlirpJ@users.noreply.github.com>
2020-12-30 19:50:54 -0600

    Update esportsParseTest.py

--- esportsParseTest.py
+++ esportsParseTest.py
@@ -3,6 +3,8 @@ import pprint
 # Joshua Prila
 # Use this for learning purposes
 
+pp = pprint.PrettyPrinter(indent=4)
+
 # Gets regions names
 regions = lp.get_regions()
 # print(regions)
@@ -25,7 +27,7 @@ games = lp.get_games(tourney[tourneyIDX])
 game = lp.get_game_details(games[0]) 
 # games[0] respresents TSM vs GG game 1 of the 2020 LCS Summer Playoffs (the first game played of the LCS Summer Playoffs)
 # Let's print out the stats of this game
-pp = pprint.PrettyPrinter(indent=4)
+
 pp.pprint(game)
 
 # Should be able to play with it from there. There are endless possibilities on using this data!
